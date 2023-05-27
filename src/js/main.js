c9ed583c9308118af7ee7a3ad5a171ae88895e9d
Andrew Gabaraev <androgabar@gmail.com>
2023-05-27 10:27:46 +0300

    pay score

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,17 @@
 const sessionObject = JSON.parse(localStorage.clientInfo);
 const scorebalance = document.querySelector(".score-balance");
 const scoreId = document.querySelector(".score-id");
+const payBtn = document.getElementById("payBtn");
+console.log(sessionObject);
+console.log(scoreId);
+console.log(scorebalance);
 const scoreName = document.querySelector(".account__score-naming");
 const sessionId = localStorage.sessionID;
 const scoreOwner = document.querySelector(".account__score-owner");
-const payScoreUrl = `http://45.84.68.38:8082/system_api/?format=json&context=web&model=users&method1=web_cabinet.add_payment_operation&arg1={"suid":\"${sessionId}\","src_ip":"10.20.30.41", "contract_number": "${scoreId}", "summa_in": "100.00", "operator": "SBERBANK_ACQ"}`;
-// scoreId.innerHTML = sessionObject.account;
-// scorebalance.innerHTML = sessionObject.balance;
-// scoreName.innerHTML = sessionObject.tariff;
-// scoreOwner.innerHTML = sessionObject.name;
+scoreId.innerHTML = sessionObject.account;
+scorebalance.innerHTML = sessionObject.balance;
+scoreName.innerHTML = sessionObject.tariff;
+scoreOwner.innerHTML = sessionObject.name;
 const payHistoryWrapper = document.getElementById("payHistoryWrapper");
 const activeServicesWrapper = document.getElementById("activeServicesWrapper");
 console.log(activeServicesWrapper);
@@ -23,6 +26,24 @@ const regexCost = /\((.*?)руб/;
 const regexName = /^[^:]+/;
 const logoutBtn = document.getElementById("logoutBtn");
 console.log(sessionId);
+
+payBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const scoreInput = document.getElementById("score-input").value;
+  console.log(scoreInput);
+  if (!scoreInput) {
+    alert("Введите сумму платежа");
+  } else {
+    const payScoreUrl = `http://45.84.68.38:8082/system_api/?format=json&context=web&model=users&method1=web_cabinet.add_payment_operation&arg1={"suid":\"${sessionId}\","src_ip":"10.20.30.41", "contract_number": "${scoreId}", "summa_in": "${scoreInput}", "operator": "SBERBANK_ACQ"}`;
+    console.log(payScoreUrl);
+    const fetchData = await fetch(
+      "https://tele-com.vip/carbon/api.php?req=" + btoa(payScoreUrl)
+    ).then((response) => console.log(response));
+    // .then((payScore) => {
+    //   return payScore;
+    // });
+  }
+});
 
 payHistoryBtn.addEventListener("click", async () => {
   allWrappers.forEach((element) => {
