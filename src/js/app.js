4e3b4db52e960c1bd35421fabaed00697627ee23
Andrew Gabaraev <46446380+Artyx71@users.noreply.github.com>
2023-06-26 19:16:46 +0300

    reload fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,169 @@
+if (localStorage.length !== 0) {
+  document.querySelector(".login-page").classList.add("dn");
+  document.querySelector(".main-page").classList.remove("dn");
+  const scorebalance = document.querySelector(".score-balance");
+  const scoreId = document.querySelector(".score-id");
+
+  const payBtn = document.getElementById("payBtn");
+  const scoreName = document.querySelector(".account__score-naming");
+  const sessionId = localStorage.sessionID;
+  console.log(sessionId);
+  const scoreOwner = document.querySelector(".account__score-owner");
+
+  const payHistoryWrapper = document.getElementById("payHistoryWrapper");
+  const activeServicesWrapper = document.getElementById(
+    "activeServicesWrapper"
+  );
+  const payHistoryBtn = document.getElementById("payHistoryBtn");
+  const paymentWrapper = document.getElementById("paymentWrapper");
+  const paymentButton = document.getElementById("paymentButton");
+  const allWrappers = document.querySelectorAll(".container-wrapper");
+  const allBtns = document.querySelectorAll(".btn");
+  const payHistoryUrl = `http://45.84.68.38:8082/system_api/?format=json&context=web&model=users&method1=web_cabinet.get_pay_log&arg1={"start_date":"01.01.2022","end_date":"01.01.2024","page_number":1,"per_page":50,"suid":\"${sessionId}\"}`;
+  const activeServicesBtn = document.getElementById("activeServicesBtn");
+  const servicesUrl = `http://45.84.68.38:8082/system_api/?format=json&context=web&model=users&method1=web_cabinet.get_usluga_list&arg1={"filter":"","get_user_uslugas_all":true,"get_user_uslugas":true,"get_setted":true,"suid":\"${sessionId}\"}`;
+  const regexCost = /\((.*?)руб/;
+  const regexName = /^[^:]+/;
+  const logoutBtn = document.getElementById("logoutBtn");
+  const sessionObject = JSON.parse(localStorage.clientInfo);
+
+  scoreId.innerHTML = sessionObject.account;
+  scorebalance.innerHTML = sessionObject.balance;
+  scoreName.innerHTML = sessionObject.tariff;
+  scoreOwner.innerHTML = sessionObject.name;
+  paymentButton.addEventListener("click", () => {
+    allWrappers.forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    allBtns.forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    paymentWrapper.classList.add("active");
+    paymentButton.classList.add("active");
+  });
+
+  payBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const scoreInput = document.getElementById("score-input").value;
+    if (!scoreInput) {
+      alert("Введите сумму платежа");
+    } else {
+      const payScoreUrl = `http://45.84.68.38:8082/system_api/?format=json&context=web&model=users&method1=web_cabinet.add_payment_operation&arg1={"suid":\"${sessionId}\","src_ip":"10.20.30.41", "contract_number": "${scoreId.innerHTML}", "summa_in": "${scoreInput}", "operator": "SBERBANK_ACQ"}`;
+
+      let payIdObject;
+
+      const res = await fetch(
+        `http://45.84.68.38:8082/system_api/?format=json&context=web&model=users&method1=web_cabinet.add_payment_operation&arg1={"suid":\"${sessionId}\","src_ip":"10.20.30.41", "contract_number": "${scoreId.innerHTML}", "summa_in": "${scoreInput}", "operator": "SBERBANK_ACQ"}`
+      );
+      payIdObject = await res.json();
+
+      const params = new URLSearchParams({
+        userName: "P1513074909-api",
+        password: "VkP@35gE",
+        amount: scoreInput * 100,
+        orderNumber: payIdObject.operation_id_out,
+        returnUrl:
+          "http://45.84.68.38/cabinet/paycheck/?paysystem=sbrf_acq&orderId=a8e38d00-6c0a-767e-8032-3c5c02480174&lang=ru",
+      });
+      await fetch(
+        `https://securepayments.sberbank.ru/payment/rest/register.do?` + params
+      )
+        .then((response) => response.json())
+        .then((payInfo) => {
+          window.open(payInfo?.formUrl, "_blank");
+        });
+    }
+  });
+
+  payHistoryBtn.addEventListener("click", async () => {
+    allWrappers.forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    allBtns.forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    payHistoryWrapper.classList.add("active");
+    payHistoryBtn.classList.add("active");
+
+    const fetchData = await fetch(
+      "https://tele-com.vip/carbon/api.php?req=" + btoa(payHistoryUrl)
+    )
+      .then((response) => response.json())
+      .then((payHistory) => {
+        console.log(payHistory);
+        return payHistory;
+      });
+
+    const tableBody = document.getElementById("payhistory-table-body");
+
+    const myChildData = fetchData.items
+      .map((element) => {
+        return `
+        <tr>
+         <th class="text-center" scope="row">${new Date(
+           element.op_date
+         ).toLocaleDateString("ru-RU")} </th>
+          <td class="text-center">${element.op_summa} ₽</td>
+       </tr>
+      `;
+      })
+      .join("");
+
+    tableBody.innerHTML = myChildData;
+  });
+
+  activeServicesBtn.addEventListener("click", async () => {
+    allWrappers.forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    allBtns.forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    activeServicesWrapper.classList.add("active");
+    activeServicesBtn.classList.add("active");
+
+    const fetchData = await fetch(
+      "https://tele-com.vip/carbon/api.php?req=" + btoa(servicesUrl)
+    )
+      .then((response) => response.json())
+      .then((serviceHistory) => {
+        return serviceHistory[1];
+      });
+    const tableBody = document.getElementById("services-table-body");
+
+    const myChildData = fetchData
+      .map((element) => {
+        const serviceName = element.__self;
+        const serviceNameResult = serviceName.match(regexName)[0];
+        const serviceCost = regexCost.exec(element.__self);
+        const serviceCostResult = serviceCost ? serviceCost[1] + "руб" : null;
+        return `
+        <tr>
+        <th scope="row">${serviceNameResult}</th>
+         <td>
+         ${new Date(element.create_date).toLocaleDateString("ru-RU")}
+        </td>
+          <td>${serviceCostResult}</td>
+       </tr>
+      `;
+      })
+      .join("");
+
+    tableBody.innerHTML = myChildData;
+  });
+
+  logoutBtn.addEventListener("click", () => {
+    location.href = "index.html";
+    localStorage.clear();
+  });
+}
 let loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", (e) => {
@@ -24,6 +190,7 @@ loginForm.addEventListener("submit", (e) => {
           const clientBalance = clientInfo.user.abonent.recomend_pay_sum;
           const clientActivationDate = clientInfo.user.abonent.activate_date;
           const clientTariff = clientInfo.user.abonent.__tarif;
+          // const sessionObject = JSON.parse(localStorage.clientInfo);
 
           const clientInfoObject = {
             name: clientName,
@@ -39,6 +206,7 @@ loginForm.addEventListener("submit", (e) => {
           document.querySelector(".login-page").classList.add("dn");
           document.querySelector(".main-page").classList.remove("dn");
           var payData = {};
+
           const sessionObject = JSON.parse(localStorage.clientInfo);
           const scorebalance = document.querySelector(".score-balance");
           const scoreId = document.querySelector(".score-id");
